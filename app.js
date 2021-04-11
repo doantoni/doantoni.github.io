ed75243dcc7b998a3a8c1595665b373cf2abdda0
doantoni <65489913+doantoni@users.noreply.github.com>
2021-04-11 22:57:17 +0200

    Corrected languages and names of projects
    after adding and moving projects names have been corrected, also changed bottom text has been correctly translated

--- app.js
+++ app.js
@@ -72,15 +72,16 @@ $(".burger").click(function(){
  hr.addEventListener("click", function(){
     titleh1.textContent = "Dobrodošli u moj web dev portfolio!";
     titleh2.textContent = "Niže su neki moji prijašnji projekti";
-    boxh1[0].textContent = "Brzinomjer";
-    boxh1[1].textContent = "Memory";
-    boxh1[2].textContent = "Semafor";
-    boxh1[3].textContent = "Pogodi boju";
-    boxh1[4].textContent = "Foto galerija";
-    boxh1[5].textContent = "Foto rešetka";
-    aboutText1.textContent = "Kao što vidite, ovi projekti su podijeljeni u dvije sekcije i svjestan sam da su neki izrazito jednostavni";
-    aboutText2.textContent = "Prva sekcija uključuje moje originalne projekte, od čega sam dva (Brzinomjer i Memory) osmislio sam, dok je zadnji (Semafor) dorađena verzija projekta sa Web Dev Bootcampa";
-    aboutText3.textContent = "Druga sekcija uključuje projekte sa Web Dev Bootcampa uz moju malu doradu";
+    boxh1[0].textContent = "Mjerač";
+    boxh1[1].textContent = "Brzinomjer";
+    boxh1[2].textContent = "Memory";
+    boxh1[3].textContent = "Semafor";
+    boxh1[4].textContent = "Pogodi boju";
+    boxh1[5].textContent = "Foto galerija";
+    boxh1[6].textContent = "Foto rešetka";
+    aboutText1.textContent = "Kao što vidite, ovi projekti su podijeljeni u dvije sekcije";
+    aboutText2.textContent = "Prva sekcija uključuje moje originalne projekte, dok druga sekcija uključuje projekte sa Web Dev Bootcampa uz moju malu doradu";
+    aboutText3.textContent = "Svjestan sam da su neki od ovih projekta iznimno jendostavni, početnički projekti, ali sam ih svejedno uključio";
     buttonText.textContent = "Nazad na vrh";
     $(".box .container button a").text("Posjetite");
 }) 
@@ -89,14 +90,15 @@ en.addEventListener("click", function(){
     titleh1.textContent = "Welcome to my web dev portfolio!"
     titleh2.textContent = "Scroll down to see my past projects";
     boxh1[0].textContent = "Gauge";
-    boxh1[1].textContent = "Memory Game";
-    boxh1[2].textContent = "Score Keeper";
-    boxh1[3].textContent = "Color Game";
-    boxh1[4].textContent = "Photo Gallery";
-    boxh1[5].textContent = "Photo Grid";
-    aboutText1.textContent = "As you can see, there are two sections for these projects, and yes I realize some of them are extremely simple.";
-    aboutText2.textContent = "The first section includes my original projects, two of which (Gauge & Memory game) I created entirely by myself, and one (Score keeeper) was a project from the Web Dev Bootcamp that I modified a little bit.";
-    aboutText3.textContent = "The second section includes projects from the Web Dev Bootcamp with minor changes made by me";
+    boxh1[1].textContent = "Speedometer";
+    boxh1[2].textContent = "Memory Game";
+    boxh1[3].textContent = "Score Keeper";
+    boxh1[4].textContent = "Color Game";
+    boxh1[5].textContent = "Photo Gallery";
+    boxh1[6].textContent = "Photo Grid";
+    aboutText1.textContent = "As you can see, these projects are separated into two sections";
+    aboutText2.textContent = "The first section includes my original projects, and the second section includes projects from the Web Dev Bootcamp with minor changes made by me";
+    aboutText3.textContent = "I realize that some of these are extremely simple beginner projects but I included them anyway";
     buttonText.textContent = "Back to top";
     $(".box .container button a").text("Visit");
 })
